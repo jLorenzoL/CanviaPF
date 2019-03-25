d9bee4c4ace56da1130b2dc64ab25cc36469eefa
Jair Lorenzo <jair.lorenzo.l@outlook.com>
2019-03-25 12:10:35 -0500

    commit

--- src/com/canvia/managed/HelloJenkins.java
+++ src/com/canvia/managed/HelloJenkins.java
@@ -3,6 +3,7 @@ package com.canvia.managed;
 public class HelloJenkins {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		String path= System.getProperty("user.dir");  
+		System.out.println(path);
 	}
 }
